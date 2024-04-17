aa1cbf9cd585d519d0614eb479f7e19e60a76b27
DeNaice <104430511+DeNaice@users.noreply.github.com>
2024-04-17 09:32:30 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ema-dkrs96-u1',
+  webDir: 'www'
+};
+
+export default config;
