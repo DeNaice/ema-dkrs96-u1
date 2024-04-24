c6a4092d93e4d030d9ae3f4aac931dfd9c2fdf22
DeNaice <104430511+DeNaice@users.noreply.github.com>
2024-04-24 11:19:12 +0200

    Ready for Abgabe

---
+++ src/app/record/record-list/record-list.page.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import {Record} from "../record.model";
+import {Statistic} from "../statistic.model";
+import {IonButton, IonContent, IonHeader, IonNav, IonTitle, IonToolbar} from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-record-list',
+  templateUrl: './record-list.page.html',
+  styleUrls: ['./record-list.page.scss'],
+  standalone: true,
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButton, IonNav]
+})
+export class RecordListPage {
+
+  records: Record[] = [];
+
+  constructor() {
+    this.records.push(
+      new Record(1, 'CS000', 'Test', 6, 69, false, true, 2010),
+      new Record(2, 'CS001', 'Test2', 6, 85, false, false, 2011)
+    );
+  }
+
+
+
+  logKennzahlen(){
+
+    const stats = new Statistic(this.records);
+
+    console.log('Anzahl der Datensätze:', stats.recordCount);
+    console.log('Anzahl der halbgewichteten Datensätze:', stats.hwCount);
+    console.log('Summe der Leistungspunkte:', stats.sumCrp);
+    console.log('Fehlende Leistungspunkte bis zum Abschluss:', stats.crpToEnd);
+    console.log('Durchschnittsnote:', stats.averageGrade);
+
+
+  }
+
+
+
+}
